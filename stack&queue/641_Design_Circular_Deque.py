13614eeae6e03f2532878096e231aad7849b37c4
Yeonjae Kim <duswokim1220@gmail.com>
2024-03-12 22:37:05 +0300

    leetcode study 1prob

---
+++ stack&queue/641_Design_Circular_Deque.py
@@ -0,0 +1,70 @@
+#애매해서 책보고 하긴 함 굳이 linked list로 해서
+import collections
+
+#왜 얘 정의를 안하고 주석을 해도 leetcode에서 통과가 되는거임? 정의를 안했는데
+#leetcode에서 제공한다
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.right = next
+        self.left = next
+
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.head, self.tail = ListNode(None), ListNode(None)
+        self.k, self.len = k, 0
+        self.head.right, self.tail.left = self.tail, self.head
+
+    def _add(self, node: ListNode, new: ListNode):
+        n = node.right
+        node.right = new
+        new.left, new.right = node, n
+        n.left = new
+    
+    def _del(self, node: ListNode):
+        n = node.right.right
+        node.right = n
+        n.left = node
+
+    def insertFront(self, value: int) -> bool:
+        if self.len == self.k:
+            return False
+        self.len += 1
+        self._add(self.head, ListNode(value))
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.len == self.k:
+            return False
+        self.len += 1
+        self._add(self.tail.left, ListNode(value))
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.len == 0:
+            return False
+        self.len -= 1
+        self._del(self.head)
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.len == 0:
+            return False
+        self.len -= 1
+        self._del(self.tail.left.left)
+        return True
+
+    def getFront(self) -> int:
+        return self.head.right.val if self.len else -1
+
+    def getRear(self) -> int:
+        return self.tail.left.val if self.len else -1
+
+    def isEmpty(self) -> bool:
+        return self.len == 0
+
+    def isFull(self) -> bool:
+        return self.len == self.k
+
+    
