c7c338d77e3323255c46b28b6c499808340b5de5
Yeonjae Kim <duswokim1220@gmail.com>
2024-03-21 11:58:58 +0300

    stduy 78 prob

--- graph/78_Subsets.py
+++ graph/78_Subsets.py
@@ -17,8 +17,26 @@ class Solution:
         dfs([], 0)
         return result
         
+
+#answer in pdf
+#이렇게 간결하게 짜고싶은데 흠
+class Solution_pdf:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def dfs(index, path):
+            #매번 결과 추가
+            result.append(path)
+
+            #경로를 만들면서 DFS
+            for i in range(index, len(nums)):
+                dfs(i + 1, path + [nums[i]])
+        
+        dfs(0, [])
+        return result
+    
 nums = [4,1,0]
-ans = Solution()
+ans = Solution_pdf()
 print(ans.subsets(nums))
 
 
