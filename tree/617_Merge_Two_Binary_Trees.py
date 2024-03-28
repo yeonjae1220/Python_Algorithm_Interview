ccf1a003f9a1d30173c05f3c33637d53c05e91dd
Yeonjae Kim <duswokim1220@gmail.com>
2024-03-28 21:55:44 +0300

    sol 617 prob

---
+++ tree/617_Merge_Two_Binary_Trees.py
@@ -0,0 +1,20 @@
+from typing import Optional
+import collections
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root1 and root2:
+            node = TreeNode(root1.val + root2.val)
+            node.left = self.mergeTrees(root1.left, root2.left)
+            node.right = self.mergeTrees(root1.right, root2.right)
+            return node
+
+        else:
+            return root1 or root2 #이렇게 리턴하는거 생각 못했네
+
+#인풋 받고 하는거 트리 짜는거 복잡해서 패스함
