05698f31b5a82865c4c9234d45db1d4a74e17255
Yeonjae Kim <duswokim1220@gmail.com>
2024-03-31 10:35:11 +0300

    study 108 prob

---
+++ tree/108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -0,0 +1,29 @@
+#pdf 봄
+from typing import Optional
+from typing import List
+import collections
+import Tree_Visualizer
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        
+        mid = len(nums) // 2
+
+        #분할 정복으로 이진 검색 결과 트리 구성
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid + 1:])
+
+        return node
+    
+nums = [-10,-3,0,5,9]
+root = Solution().sortedArrayToBST(nums)
+Tree_Visualizer.preorder(root)
