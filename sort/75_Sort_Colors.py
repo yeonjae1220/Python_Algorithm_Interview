006e54bb62dfb5eb9238261ca6d33aa82471f6bf
Yeonjae Kim <duswokim1220@gmail.com>
2024-04-13 18:51:26 +0300

    add 75prob pdf ans

--- sort/75_Sort_Colors.py
+++ sort/75_Sort_Colors.py
@@ -13,10 +13,30 @@ class Solution:
                 j -= 1
             i += 1
 
-        
+        return nums
+    
+#Dutch National Flag Problem
+class Solution_pdf:
+    def sortColors(self, nums: List[int]) -> List[int]:# None:
+        # red, white, blue = 0, 0, len(nums)
 
+        # while white < blue:
+        #     if 
+
+        i, j, k = 0, 0, len(nums)
+        while j < k:
+            if nums[j] < 1:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j += 1
+            elif nums[j] > 1:
+                k -= 1
+                nums[j], nums[k] = nums[k], nums[j]
+            else:
+                j +=1
+        
         return nums
     
 nums = [2,0,2,1,1,0]
-print(Solution().sortColors(nums))
+print(Solution_pdf().sortColors(nums))
         
