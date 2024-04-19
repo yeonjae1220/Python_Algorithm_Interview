d850482f652ca94445ef90d9e0d6e170dca79495
Yeonjae Kim <duswokim1220@gmail.com>
2024-04-19 23:46:40 +0300

    temp

---
+++ binary_search/240_Search_a_2D_Matrix_2.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # def findMatrix(matrix: List[List[int]]):
+        #     temp = max(len(matrix), len(matrix[0]))
+        #     for i in range(temp):
+        #         if target < matrix[i][i]:
+
+        temp = max(len(matrix), len(matrix[0]))
+
+
+
+        
+
+
+
+
+matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+target = 5
